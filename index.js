f98c17108d955bff46a9027ea2814fa9accf0d70
Mikhail Arden <mikhail@macromade.com>
2020-09-04 17:23:56 -0500

    My first commit

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+app.get('/cart_script.js', (req, res) => {
+    res.sendFile('./public/cart_script.js', { root: __dirname });
+});
