98de49e6bf15f2747c7266242463afd822540d13
Mikhail Arden <mikhail@macromade.com>
2020-09-04 18:27:38 -0500

    My first commit

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ express()
     .get('/cart.js', (req, res) => res.sendFile(__dirname + '/public/cart_function.js'))
     .get('/production/cart.js', function(req, res) {
         res.render(__dirname + '/public/cart_function.js', { token: client_key });
-    });
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 require('dotenv').config()
 
