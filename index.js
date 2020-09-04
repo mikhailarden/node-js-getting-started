2a767c997d6b673ff69e560f950a7a49bb70221a
Mikhail Arden <mikhail@macromade.com>
2020-09-04 17:52:59 -0500

    My first commit

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('pages/index'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
     .get('/cart.js', (req, res) => res.sendfile(__dirname + '/public/cart_function.js'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
 console.log('Server listening on port 9000');
