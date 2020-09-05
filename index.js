c85800488f6f49e4e53766a4fcb3908b71ca20bf
Mikhail Arden <mikhail@macromade.com>
2020-09-05 11:52:29 -0500

    My first commit

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
     .get('/cart.js', (req, res) => res.sendFile(__dirname + '/public/cart_function.js'))
     .get('/production/cart.js', function(req, res) {
-        res.render(__dirname + '/public/cart_function.js', { token: client_key } { pass: client_pass } { order_auth: order_auth });
+        res.render(__dirname + '/public/cart_function.js', { token: client_key }, { pass: client_pass }, { order_auth: order_auth });
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 require('dotenv').config()
 
-console.log('Server listening on port 9000');
+console.log('Server listening on port 9000 test');
