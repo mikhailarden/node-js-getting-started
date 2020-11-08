2b32efae1e8c6bf83b6dc3046ec253a3438c5165
Mikhail Arden <mikhail@macromade.com>
2020-11-07 23:19:27 -0500

    ......

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ var axios = require('axios');
 
 // Config modules
 
-var storeURL = "https://comfortgroup.myshopify.com/admin/";
-var apiVersion = "2020-07";
+var storeURL = "https://bijoux-medusa.myshopify.com/admin/";
+var apiVersion = "2020-10";
 
 // API Config
-    
-    var apiPass = process.env.BASIC_AUTH;
-    var clientPass = process.env.CLIENT_PASS;
-    var productAuth = process.env.CLIENT_KEY; // confusing naming convention.
-    
+    var accessToken = process.env.ACCESS_TOKEN;
+    var AuthKey = process.env.AUTH_KEY;
+   
 //Runs every time a request is recieved
 function logger(req, res, next) {
     console.log('Request from: ' + req.ip + ' For: ' + req.path); //Log the request to the console
@@ -28,14 +26,28 @@ function logger(req, res, next) {
 }
 
 // Draft Order function
-function draftOrder(lineJSON) {
+function productJSON(productID) {
+    var config = {
+      method: 'get',
+      url: 'https://bijoux-medusa.myshopify.com/admin/api/2020-10/products/'+productID+'.json',
+      headers: { 
+        'X-Shopify-Access-Token': accessToken, 
+        'Content-Type': 'application/json', 
+        'Authorization': AuthKey
+      }
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
-
 
 var port = 8000;
 var app = express(); //Initialize the app
-
-
 
 //Configure the settings of the app
 app.use(cors()); // Enables CORS
@@ -51,10 +63,18 @@ app.use(function(req, res, next) {
 });
 
 //Dynamic get handler
-app.route('/draft_order')
+app.route('/inventory')
+.get(function(req, res) {
+    res.setHeader('Content-Type', 'application/json'); //Tell the client you are sending plain text
+    res.end(req.cookies);
+    console.log(req)
+   
+})
+
+app.route('/product')
 .get(function(req, res) {
 console.log('Page is active')
-    res.setHeader('Content-Type', 'text/plain'); //Tell the client you are sending plain text
+    res.setHeader('Content-Type', 'application/json'); //Tell the client you are sending plain text
     res.end(req.cookies);
    
 })
