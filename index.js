b047c6d45a93a7c28b6fd2a3e9becc9011787ef7
Mikhail Arden <mikhail@macromade.com>
2020-11-07 23:41:00 -0500

    ...

--- index.js
+++ index.js
@@ -16,39 +16,13 @@ var storeURL = "https://bijoux-medusa.myshopify.com/admin/";
 var apiVersion = "2020-10";
 
 // API Config
-    var accessToken = process.env.ACCESS_TOKEN;
-    var AuthKey = process.env.AUTH_KEY;
-   
+var accessToken = process.env.ACCESS_TOKEN;
+var AuthKey = process.env.AUTH_KEY;
+
 //Runs every time a request is recieved
 function logger(req, res, next) {
     console.log('Request from: ' + req.ip + ' For: ' + req.path); //Log the request to the console
     next(); //Run the next handler (IMPORTANT, otherwise your page won't be served)
-}
-
-// Draft Order function
-function productJSON(productID) {
-    var productJSON;
-    
-    var config = {
-      method: 'get',
-      url: 'https://bijoux-medusa.myshopify.com/admin/api/2020-10/products/'+productID+'.json',
-      headers: { 
-        'X-Shopify-Access-Token': accessToken, 
-        'Content-Type': 'application/json', 
-        'Authorization': AuthKey
-      }
-    };
-    
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      productJSON = JSON.stringify(response.data);
-      
-      return productJSON
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
 }
 
 var port = 8000;
@@ -59,7 +33,7 @@ app.use(cors()); // Enables CORS
 app.use(logger); //Tells the app to send all requests through the 'logger' function
 app.use(express.static('/public')); //Tells the app to serve static files from ./public_html/
 app.use(express.json());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -69,36 +43,44 @@ app.use(function(req, res, next) {
 
 //Dynamic get handler
 app.route('/inventory')
-.get(function(req, res) {
-    res.setHeader('Content-Type', 'application/json'); //Tell the client you are sending plain text
-    res.end(req.cookies);
-    console.log(req)
-})
-.post(function(req, res) {
+    .get(function (req, res) {
+        res.setHeader('Content-Type', 'application/json'); //Tell the client you are sending plain text
+        res.end(req.cookies);
+        console.log(req)
+    })
+    .post(function (req, res) {
 
-    
-})
+
+    })
 
 app.route('/product')
-.get(function(req, res) {
-console.log('Page is active')
-    res.setHeader('Content-Type', 'application/json'); //Tell the client you are sending plain text
-    res.end(req.cookies);
-   
-})
-.post(function(req, res) {
-    console.log(req.body)
-    var productID = req.body.product;
-    productJSON(productID) 
-    
-    axios(productJSON(productID))
-    .then(function (response) {
-        var result = JSON.stringify(response.data);
-        res.json(result)
-    })
-    .catch(function (error) {
-        res.json(error)
-    });
-});
+    .get(function (req, res) {
+        console.log('Page is active')
+        res.setHeader('Content-Type', 'application/json'); //Tell the client you are sending plain text
+        res.end(req.cookies);
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    })
+    .post(function (req, res) {
+        console.log(req.body)
+        var productID = req.body.product,
+            config = {
+                method: 'get',
+                url: 'https://bijoux-medusa.myshopify.com/admin/api/2020-10/products/' + productID + '.json',
+                headers: {
+                    'X-Shopify-Access-Token': accessToken,
+                    'Content-Type': 'application/json',
+                    'Authorization': AuthKey
+                }
+            };
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                var result = JSON.stringify(response.data);
+                res.json(result)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    });
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
